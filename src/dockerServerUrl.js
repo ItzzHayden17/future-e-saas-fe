a71cf9d708b4c67ca72e6bf0fafe2086ab9cb8df
ItzzHayden17 <sony.anray743@gmail.com>
2026-07-07 16:39:48 +0200

    changing document server

--- src/dockerServerUrl.js
+++ src/dockerServerUrl.js
@@ -1,3 +1,3 @@
-export const dockerServerUrl = 'http://localhost:8081'
-//Actuall
 // export const dockerServerUrl = 'http://localhost:8081'
+//Actuall
+export const dockerServerUrl = 'https://documentserver-latest-523p.onrender.com'
